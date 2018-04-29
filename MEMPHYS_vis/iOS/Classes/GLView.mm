f080f3633a5602c4cd611070fca5cb9548ce8cba
Guy Barrand <guy.barrand@gmail.com>
2018-04-29 10:08:46 +0200

    update

---
+++ MEMPHYS_vis/iOS/Classes/GLView.mm
@@ -0,0 +1,12 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file MEMPHYS_vis.license for terms.
+
+#import <UIKit/UIKit.h>
+
+#undef pascal
+
+#include "../../MEMPHYS_vis/main"
+
+#define EXLIB_APP MEMPHYS_vis
+
+#include <exlib/app/iOS/GLView_mm>
